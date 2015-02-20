49e3fd2b3123e3d1d850a3cecca51fcf7136f081
hemdagem <hemdagem@localhost>
2015-02-20 11:02:02 +0000

    updated voice recognition script

--- VoiceRecognition/Scripts/script.js
+++ VoiceRecognition/Scripts/script.js
@@ -3,21 +3,20 @@
     var span = document.getElementById("outputSpan");
 
     var recognition = new webkitSpeechRecognition();
-    //recognition.continuous = true;
+    recognition.continuous = true;
     recognition.lang = "en-GB";
 
     recognition.onresult = function (event) {
+
         console.log(event);
 
         if (event.results.length == 0) {
             return;
         }
 
-        for (var i = 0; i < event.results.length; i++) {
+        var currentEvent = event.results[event.results.length - 1];
+        span.innerHTML += currentEvent[0].transcript + " ";
 
-            var currentEvent = event.results[i];
-            span.innerHTML += currentEvent[0].transcript + " ";
-        }
 
     };
     recognition.start();
