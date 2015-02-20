aac4791433c7dd0c81689249a7d03dd549b61bea
hemdagem <hemdagem@localhost>
2015-02-20 10:41:47 +0000

    initial project created with speech setup recognised on page

---
+++ VoiceRecognition/Scripts/script.js
@@ -0,0 +1,25 @@
+﻿(function () {
+
+    var span = document.getElementById("outputSpan");
+
+    var recognition = new webkitSpeechRecognition();
+    //recognition.continuous = true;
+    recognition.lang = "en-GB";
+
+    recognition.onresult = function (event) {
+        console.log(event);
+
+        if (event.results.length == 0) {
+            return;
+        }
+
+        for (var i = 0; i < event.results.length; i++) {
+
+            var currentEvent = event.results[i];
+            span.innerHTML += currentEvent[0].transcript + " ";
+        }
+
+    };
+    recognition.start();
+
+})();
