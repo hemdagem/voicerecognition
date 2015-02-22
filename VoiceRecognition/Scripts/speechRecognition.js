27079ec0088909526c1b27de038858f91cf15496
hemdagem <hemdagem@localhost>
2015-02-22 14:08:00 +0000

    refactor

--- VoiceRecognition/Scripts/speechRecognition.js
+++ VoiceRecognition/Scripts/speechRecognition.js
@@ -4,10 +4,12 @@
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.lang = "en-GB";
-    
+
+    var dialogActive = false;
+
     function returnResult(results) {
         var currentEvent = results[results.length - 1];
-        speechOutput += currentEvent[0].transcript + " ";
+        speechOutput += currentEvent[0].transcript;
     };
 
     function viewSpeechResults() {
@@ -18,19 +20,33 @@
     function searchTotalJobs() {
         if (speechOutput.length > 0) {
             var keywords = encodeURI(speechOutput);
+            Config.StopRecording();
             window.open("http://www.totaljobs.com/JobSearch/Results.aspx?Keywords=" + keywords);
+            $('#myModal').modal('hide');
         }
+    }
+
+    function launchDialog() {
+        $('#myModal').modal('show');
+        speechOutput = "";
+        dialogActive = true;
     }
 
     recognition.onresult = function (event) {
 
         console.log(event);
-
+        
         if (event.results.length === 0) {
             return;
         }
 
         returnResult(event.results);
+
+        if (dialogActive === false && speechOutput === "total jobs") {
+            launchDialog();
+            return;
+        }
+
         viewSpeechResults();
         searchTotalJobs();
     };
@@ -47,5 +63,7 @@
     Config.StopRecording = function () {
         recognition.stop();
     };
+    
+    Config.StartRecording();
 
 })();
